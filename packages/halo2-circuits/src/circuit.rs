a88b3545dcbbfc4baece39530ff90bbcb59e00ea
chokermaxx <135603985+chokermaxx@users.noreply.github.com>
2023-11-05 19:54:25 +0900

    Make the test pass with LIMB_BITS=32

--- packages/halo2-circuits/src/circuit.rs
+++ packages/halo2-circuits/src/circuit.rs
@@ -1,13 +1,13 @@
 use halo2_base::{
-    gates::{circuit::builder::BaseCircuitBuilder, GateInstructions, RangeChip, RangeInstructions},
+    gates::{circuit::builder::BaseCircuitBuilder, GateInstructions, RangeInstructions},
     halo2_proofs::{arithmetic::Field, halo2curves::bn256::Fr},
     poseidon::hasher::{spec::OptimizedPoseidonSpec, PoseidonHasher},
     AssignedValue, Context,
 };
 use halo2_ecc::bigint::OverflowInteger;
 use halo2_rsa::{
-    AssignedBigUint, AssignedRSAPubE, AssignedRSAPublicKey, AssignedRSASignature, BigUintConfig, BigUintInstructions,
-    Fresh, RSAConfig, RSAInstructions, RSAPubE, RSAPublicKey, RSASignature,
+    AssignedBigUint, AssignedRSAPubE, AssignedRSAPublicKey, BigUintConfig, BigUintInstructions, Fresh, RSAConfig,
+    RSAInstructions, RSAPubE, RSAPublicKey, RSASignature,
 };
 use num_bigint::BigUint;
 use num_traits::{identities::One, ToPrimitive};
@@ -35,7 +35,7 @@ const E: usize = 65537;
 // a) 2048, the width of RSA keys, must be able to divide limb_bits https://github.com/zkemail/halo2-rsa/blob/main/src/big_uint/chip.rs#L49C49-L49C49
 // b) limb_bits * 2 must be smaller than 253 the order of the field https://github.com/axiom-crypto/halo2-lib/blob/dd21d6c2ddb1c6cb5ef78f20d68a6c9682353698/halo2-ecc/src/bigint/mul_no_carry.rs#L8
 // the maximum value that satisfies the both is 64
-const LIMB_BITS: usize = 64;
+const LIMB_BITS: usize = 32;
 
 // halo2_rsa's mul_mod constraints are too heavy for just taking a bit slice.
 // here I write optimized constraints that constraints the same thing.
@@ -57,7 +57,6 @@ fn slice_biguint(
     // beginning_part:      |-|  |-|  |-|  |-|
     //    ending_part:        |--| |--| |--| |-|
     //     final_part:                         ||
-    //
 
     let beginning_part_width = since % biguint_chip.limb_bits();
     let ending_part_base = ctx.load_constant(Fr::from(2).pow_vartime([beginning_part_width as u64]));
@@ -68,27 +67,26 @@ fn slice_biguint(
     let mut parts: Vec<AssignedValue<Fr>> = Vec::new();
 
     // assign witnesses
-    for (i, src_limb) in src.value().iter_u64_digits().enumerate() {
-        let do_src_and_dest_overlap =
-            since < (i + 1) * biguint_chip.limb_bits() && until > i * biguint_chip.limb_bits();
-        if !do_src_and_dest_overlap {
-            continue;
-        }
+    let mut read_bits = since / biguint_chip.limb_bits() * biguint_chip.limb_bits();
+    while read_bits < until {
+        let src_limb = (src.value() >> read_bits) % (BigUint::one() << biguint_chip.limb_bits());
 
-        let beginning_part_max_width = until - i * biguint_chip.limb_bits();
+        let beginning_part_max_width = until - read_bits;
         let beginning_part_width = beginning_part_width.min(beginning_part_max_width);
-        let beginning_part = BigUint::from(src_limb) % (BigUint::one() << beginning_part_width);
+        let beginning_part = src_limb.clone() % (BigUint::one() << beginning_part_width);
         parts.push(ctx.load_witness(Fr::from(beginning_part.to_u64().unwrap())));
 
-        let ending_part_max_width = until - i * biguint_chip.limb_bits() - beginning_part_width;
+        let ending_part_max_width = until - read_bits - beginning_part_width;
         let ending_part_width = ending_part_width.min(ending_part_max_width);
-        let ending_part = (BigUint::from(src_limb) >> beginning_part_width) % (BigUint::one() << ending_part_width);
+        let ending_part = (src_limb.clone() >> beginning_part_width) % (BigUint::one() << ending_part_width);
         parts.push(ctx.load_witness(Fr::from(ending_part.to_u64().unwrap())));
 
-        if until < (i + 1) * biguint_chip.limb_bits() {
-            let final_part = src_limb >> final_part_offset;
-            parts.push(ctx.load_witness(Fr::from(final_part)));
+        if until < read_bits + biguint_chip.limb_bits() {
+            let final_part = src_limb.clone() >> final_part_offset;
+            parts.push(ctx.load_witness(Fr::from(final_part.to_u64().unwrap())));
         }
+
+        read_bits += biguint_chip.limb_bits();
     }
 
     // range check the first beginning part
