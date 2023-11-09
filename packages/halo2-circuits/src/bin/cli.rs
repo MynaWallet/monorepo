44f8aeb16f91a3209a9dbcd4f35d0d244613167b
chokermaxx <135603985+chokermaxx@users.noreply.github.com>
2023-11-09 15:33:33 +0900

    Measure the gas cost

--- packages/halo2-circuits/src/bin/cli.rs
+++ packages/halo2-circuits/src/bin/cli.rs
@@ -74,6 +74,28 @@ enum Commands {
         issuer_cert_path: String,
         password: u64,
     },
+    /// Generate the proving key and the verification key for RSA circuit
+    GenerateSolidity {
+        /// k parameter for circuit.
+        #[arg(long, default_value = "17")]
+        k: u32,
+        /// trusted setup parameters path. input
+        #[arg(short, long, default_value = "./params")]
+        params_path: String,
+        /// proving key path. output
+        #[arg(long, default_value = "./build/rsa.pk")]
+        pk_path: String,
+        /// proof path. output
+        #[arg(long, default_value = "./build/myna_verify_rsa.proof")]
+        proof_path: String,
+        // citizen's certificate
+        #[arg(long, default_value = "./certs/myna_cert.pem")]
+        verify_cert_path: String,
+        // nation's certificate
+        #[arg(long, default_value = "./certs/ca_cert.pem")]
+        issuer_cert_path: String,
+        password: u64,
+    },
 }
 
 fn main() {
@@ -93,7 +115,7 @@ fn main() {
             password,
         } => {
             let nation_pubkey = read_nation_cert(&issuer_cert_path);
-            let (nation_sig, tbs_cert, citizen_pubkey) = read_citizen_cert(&verify_cert_path);
+            let (nation_sig, tbs_cert, _citizen_pubkey) = read_citizen_cert(&verify_cert_path);
 
             let mut builder = BaseCircuitBuilder::new(false);
             builder.set_k(k as usize);
@@ -154,56 +176,57 @@ fn main() {
             let trusted_setup = gen_srs(k);
             let pk = read_pk::<BaseCircuitBuilder<Fr>>(Path::new(&pk_path), builder.params()).unwrap();
             gen_snark_shplonk(&trusted_setup, &pk, builder, Some(Path::new(&proof_path)));
-        } /* Commands::GenRsaVerifyEVMProof {
-           *     k,
-           *     params_path,
-           *     pk_path,
-           *     verify_cert_path,
-           *     issuer_cert_path,
-           *     proof_path,
-           * } => {
-           *     env::set_var("PARAMS_DIR", params_path);
-           *     let params = gen_srs(k); */
+        }
+        Commands::GenerateSolidity {
+            k,
+            params_path,
+            pk_path,
+            proof_path,
+            verify_cert_path,
+            issuer_cert_path,
+            password,
+        } => {
+            let nation_pubkey = read_nation_cert(&issuer_cert_path);
+            let (nation_sig, tbs_cert, citizen_pubkey) = read_citizen_cert(&verify_cert_path);
 
-          /*     let (tbs, signature_bigint) = extract_tbs_and_sig(&verify_cert_path);
-           *     let public_key_modulus = extract_public_key(&issuer_cert_path); */
+            let mut builder = BaseCircuitBuilder::new(false);
+            builder.set_k(k as usize);
+            builder.set_lookup_bits(circuit::LOOKUP_BITS);
+            builder.set_instance_columns(1);
+            let range_chip = builder.range_chip();
+            let ctx = builder.main(0);
 
-          /*     let builder = create_default_rsa_circuit_with_instances(
-           *         k as usize,
-           *         tbs,
-           *         public_key_modulus,
-           *         signature_bigint,
-           *     );
-           *     let pk =
-           *         read_pk::<BaseCircuitBuilder<Fr>>(Path::new(&pk_path), builder.params()).unwrap(); */
+            let public = circuit::PublicInput { nation_pubkey };
+            let private =
+                circuit::PrivateInput { tbs_cert: tbs_cert.to_bytes_le(), nation_sig, password: Fr::from(password) };
+            let outputs = circuit::proof_of_japanese_residence(ctx, range_chip, public, private);
 
-          /*     if Path::new(&proof_path).exists() {
-           *         match remove_file(&proof_path) {
-           *             Ok(_) => println!("File found, overwriting..."),
-           *             Err(e) => println!("An error occurred: {}", e),
-           *         }
-           *     }
-           *     gen_snark_shplonk(&params, &pk, builder.clone(), Some(Path::new(&proof_path))); */
+            builder.assigned_instances[0].extend(outputs);
+            let circuit_params = builder.calculate_params(None);
+            builder = builder.use_params(circuit_params);
 
-          /*     let deployment_code = gen_evm_verifier_shplonk::<BaseCircuitBuilder<Fr>>(
-           *         &params,
-           *         pk.get_vk(),
-           *         builder.num_instance(),
-           *         Some(Path::new("./build/VerifyRsa.sol")),
-           *     ); */
+            env::set_var("PARAMS_DIR", params_path);
+            let trusted_setup = gen_srs(k);
+            let pk = read_pk::<BaseCircuitBuilder<Fr>>(Path::new(&pk_path), builder.params()).unwrap();
 
-          /*     let proof = gen_evm_proof_shplonk(&params, &pk, builder.clone(), builder.instances()); */
+            let deployment_code = gen_evm_verifier_shplonk::<BaseCircuitBuilder<Fr>>(
+                &trusted_setup,
+                pk.get_vk(),
+                builder.num_instance(),
+                Some(Path::new("./build/VerifyRsa.sol")),
+            );
 
-          /*     println!("Size of the contract: {} bytes", deployment_code.len());
-           *     println!("Deploying contract..."); */
+            let proof = gen_evm_proof_shplonk(&trusted_setup, &pk, builder.clone(), builder.instances());
 
-          /*     evm_verify(deployment_code, builder.instances(), proof.clone()); */
+            println!("Size of the contract: {} bytes", deployment_code.len());
+            println!("Deploying contract...");
 
-          /*     println!("Verification success!"); */
+            evm_verify(deployment_code, builder.instances(), proof.clone());
 
-          /*     write_calldata(&builder.instances(), &proof, Path::new("./build/calldata.txt")).unwrap();
-           *     println!("Succesfully generate calldata!"); */
+            println!("Verification success!");
 
-          /* } */
+            write_calldata(&builder.instances(), &proof, Path::new("./build/calldata.txt")).unwrap();
+            println!("Succesfully generate calldata!");
+        }
     }
 }
